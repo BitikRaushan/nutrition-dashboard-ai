72a1d29a624638a79949f9b95e6156f06af0dba5
BitikRaushan <bitikraushan3057@gmail.com>
2026-03-20 15:32:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from model import calculate_nutrition
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -23,5 +24,8 @@ def calculate():
     return jsonify(result)
 
 
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
