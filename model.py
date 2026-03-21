0e71601769b1d1902569e7db402be3297af9c49b
BitikRaushan <bitikraushan3057@gmail.com>
2026-03-21 20:50:05 +0530

    Refactor nutrient calculations with qty factor
    Adjusted nutrient calculations to use a quantity factor based on 100g servings.

--- model.py
+++ model.py
@@ -27,11 +27,13 @@ def calculate_nutrition(weight, foods):
         row = df[df["Dish Name"].str.lower().str.contains(food.lower())]
 
         if not row.empty:
-            daily_total["protein"] += row["Protein (g)"].values[0] * qty
-            daily_total["carbs"] += row["Carbohydrates (g)"].values[0] * qty
-            daily_total["fat"] += row["Fats (g)"].values[0] * qty
-            daily_total["calories"] += row["Calories (kcal)"].values[0] * qty
-            daily_total["iron"] += row["Iron (mg)"].values[0] * qty
+            qty_factor = qty / 100  # 🔥 VERY IMPORTANT
+
+           daily_total["protein"] += row["Protein (g)"].values[0] * qty_factor
+           daily_total["carbs"] += row["Carbohydrates (g)"].values[0] * qty_factor
+           daily_total["fat"] += row["Fats (g)"].values[0] * qty_factor
+           daily_total["calories"] += row["Calories (kcal)"].values[0] * qty_factor
+           daily_total["iron"] += row["Iron (mg)"].values[0] * qty_factor
 
     # ===== SUGGESTIONS =====
     suggestions = {}
